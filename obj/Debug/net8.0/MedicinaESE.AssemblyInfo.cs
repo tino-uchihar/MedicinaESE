7fa35177540e53c62f557edfbd4cd5f6b814f4af
Tino Uchihar <frenteria1@udi.edu.co>
2025-04-21 16:55:10 -0500

    wea de texto (tamal)

--- obj/Debug/net8.0/MedicinaESE.AssemblyInfo.cs
+++ obj/Debug/net8.0/MedicinaESE.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MedicinaESE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf145f92c93cedd6188dcbe24a5efc743c0116a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6726292a5a7910121f9f500508765a286eef285e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MedicinaESE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MedicinaESE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
